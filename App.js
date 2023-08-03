c19a330c25f80ce56130dace2990edcc1b0155bb
ntantai2526 <ntantai2526@gmail.com>
2023-08-03 21:49:18 +0700

    MovieDetail - ts - update: gọi lấy imdbID qua bên Showtimes để so sánh và lấy thông tin trong Showtimesdb, app.js câp nhật các thứ cần thiết

--- App.js
+++ App.js
@@ -8,6 +8,10 @@ import Register from './Pages/Register'
 import Showtimes from './Pages/Showtimes'
 import TicketBookingScreen from './Pages/TicketBookingScreen'
 import Payment from './Pages/Payment'
+// import FileGet from './Pages/FileGet'
+// import FileSave from './Pages/FileSave'
+
+
 // import Datepicker from './Pages/datepicker'
 
 
@@ -16,7 +20,7 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer >
-      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='Showtimes'>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='Register'>
         <Stack.Screen name='Movie Explorer' component={MovieDisplay}/>
         <Stack.Screen name='Movie Detail' component= {MovieDetail } />
         <Stack.Screen name='Showtimes' component= {Showtimes} />
@@ -24,6 +28,8 @@ const App = () => {
         <Stack.Screen name='Register' component= {Register} />
         <Stack.Screen name='TicketBooking' component= {TicketBookingScreen} />
         <Stack.Screen name='Payment' component= {Payment} />
+        {/* <Stack.Screen name='FileGet' component= {FileGet} />
+        <Stack.Screen name='FileSave' component= {FileSave} /> */}
         {/* <Stack.Screen name='Datepicker' component= {Datepicker}/> */}
       </Stack.Navigator>
     </NavigationContainer>
